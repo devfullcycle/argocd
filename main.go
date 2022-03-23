a7608658393499ba1cf6a62c3be25405ff07d0dc
Wesley Willians <wesleywillians@gmail.com>
2022-03-23 18:26:22 -0400

    foi

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>Hello Full Cycle!!!</h1>"))
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8081", nil)
 }
