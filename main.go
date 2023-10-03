295670fd0b7fa7de6688fa5783e025430240e9f3
Wesley <wesleywillians@gmail.com>
2023-10-03 15:41:48 -0400

    add promhttp

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	// add prometheus
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>Hello MBA Full Cycle!!!!!!</h1>"))
 	})
+	// add promhttp.Handler()
+	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
 }
