da78c4d3560b1ea98988aea2cbd75fd2fc07d442
Luiz Carlos <argentinaluiz@gmail.com>
2023-10-03 16:32:19 -0300

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello Full Cycle!!!!!!</h1>"))
+		w.Write([]byte("<h1>Hello MBA Full Cycle!!!!!!</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
